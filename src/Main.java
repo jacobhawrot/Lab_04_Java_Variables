5764cc32c06fd358687f5007af0daec6e6a6d9ed
Jacob Hawrot <jacobhawrot@gmail.com>
2025-09-15 20:48:21 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int intOperandA = 24;
+        int intOperandB = 5;
+        int intSum, intProduct, intDifference, intQuotient, intModulo;
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " + " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " * " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using ints of " + intOperandA + " - " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of " + intOperandA + " / " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using ints of " + intOperandA + " % " + intOperandB + " is " + intModulo);
+
+        System.out.println();
+
+        double doubleOperandA = 24.0;
+        double doubleOperandB = 5.0;
+        double doubleSum, doubleProduct, doubleDifference, doubleQuotient;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " + " + doubleOperandB + " is " + doubleSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " * " + doubleOperandB + " is " + doubleProduct);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " - " + doubleOperandB + " is " + doubleDifference);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " / " + doubleOperandB + " is " + doubleQuotient);
+
+        System.out.println();
+
+        double myLunchCost = 12.50;
+        int numberOfKidsInFamily = 0;
+        boolean isRaining = false;
+        double gasPricePerGallon = 3.59;
+        int favoriteNumber = 7;
+        double shoeSize = 10.5;
+        String birthMonth = "September";
+        String fullName = "Jacob Hawrot";
+
+        System.out.println("The cost of my lunch is: " + myLunchCost);
+        System.out.println("Number of kids in my family: " + numberOfKidsInFamily);
+        System.out.println("Is it raining? " + isRaining);
+        System.out.println("Price of a gallon of gas: " + gasPricePerGallon);
+        System.out.println("My favorite number: " + favoriteNumber);
+        System.out.println("My shoe size: " + shoeSize);
+        System.out.println("My birth month: " + birthMonth);
+        System.out.println("My full name: " + fullName);
     }
 }
